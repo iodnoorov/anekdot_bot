420d8d253fa0c2b177dc5accd7cc40a6a2fc11c6
Ivan Odnoorov <elcheshirecat@yandex.ru>
2024-12-24 20:32:52 +0300

    Initial commit

---
+++ anekdot.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+st_accept = "text/html"
+st_useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_3_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
+
+headers = {
+   "Accept": st_accept,
+   "User-Agent": st_useragent
+}
+
+
+def get_new_anekdot():
+    req = requests.get("https://www.anekdot.ru/random/anekdot/", headers)
+
+    src = req.text
+    #print(src)
+
+    soup = BeautifulSoup(src, 'lxml')
+
+    anekdot = soup.find('div', class_='text').text
+
+    return anekdot
---
+++ bot.py
@@ -0,0 +1,42 @@
+from config import BOT_TOKEN 
+from telebot import TeleBot
+from anekdot import get_new_anekdot
+
+bot = TeleBot(token=BOT_TOKEN)
+
+help_message = """\
+Я умею:
+/start - выводит приветственное сообщение
+/help - узнать, что я умею
+/anekdot - вывод случайного анекдота
+"""
+
+warning = """
+Анекдоты, как и большинство шуток в интернете, могут быть 'черным юмором'.
+Заранее извинияюсь за возможное оскорбительное содержание.
+"""
+
+podskaska = """
+Чтобы узнать, что я умею введи /help"""
+@bot.message_handler(commands=['start'])
+def bot_start(message):
+    bot.send_message(message.chat.id, text=f"Привет, {message.from_user.first_name}!" + warning + podskaska)
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(message.chat.id, text=help_message )
+
+def send_random_anekdot(message):
+    anekdot = get_new_anekdot()
+    bot.send_message(message.chat.id, text=anekdot)
+    bot.send_message(message.chat.id, text="ещё один анекдот? /anekdot")
+bot.message_handler(commands=['anekdot'])(send_random_anekdot)
+
+@bot.message_handler(func=lambda m: True)
+def unknown_command(message):
+ known_commands = ['/start', '/help', '/person', '/news']
+ if message.text.split()[0] not in known_commands:
+     bot.reply_to(message, "Некорректный ввод. Введите /help для просмотра доступных функций")
+     
+bot.polling()
+
